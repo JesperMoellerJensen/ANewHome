2267faecd1648329f842fc56272060e3029dc0ad
Martin Fabricius <jack.corp@hotmail.com>
2019-04-01 12:01:11 +0200

    IInventory

---
+++ ANewHomeGame/Assets/Scripts/IInventory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public interface IInventory
+{
+	IObtainable AddItem(IObtainable item, int index);
+	IObtainable AddItem(IObtainable item);
+	IObtainable RemoveItem(int index);
+}
